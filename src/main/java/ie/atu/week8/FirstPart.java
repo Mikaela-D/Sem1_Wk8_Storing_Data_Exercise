82164b9a3b54177552667fef3ad18337d5d6b4a1
Mikaela <G00401392@atu.ie>
2022-11-15 14:15:22 +0000

    New branch with second and third parts of the exercise

--- src/main/java/ie/atu/week8/FirstPart.java
+++ src/main/java/ie/atu/week8/FirstPart.java
@@ -5,8 +5,16 @@ import java.io.*;
 public class FirstPart {
     public static void main(String[] args) {
         {
-            File myFile = new File("myFile.txt");
+            File myFile = new File("myFile1.txt");
             System.out.println("My file is located at " + myFile.getAbsolutePath());
+            String content = "Details to write";
+            try {
+                FileWriter myWriter = new FileWriter(myFile, true);
+                myWriter.write("Hello world");
+                myWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
