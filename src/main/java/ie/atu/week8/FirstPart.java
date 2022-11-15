92dd7fc0b817374f41f84619cb132712f6d5d7d1
Mikaela <G00401392@atu.ie>
2022-11-15 13:29:27 +0000

    New branch with first part of the exercise

--- src/main/java/ie/atu/week8/FirstPart.java
+++ src/main/java/ie/atu/week8/FirstPart.java
@@ -1,4 +1,12 @@
 package ie.atu.week8;
 
+import java.io.*;
+
 public class FirstPart {
+    public static void main(String[] args) {
+        {
+            File myFile = new File("myFile.txt");
+            System.out.println("My file is located at " + myFile.getAbsolutePath());
+        }
+    }
 }
