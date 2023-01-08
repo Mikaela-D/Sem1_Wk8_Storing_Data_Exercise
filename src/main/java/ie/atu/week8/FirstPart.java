14da47feeea28322b75e3c27a158695115e24749
Mikaela <G00401392@atu.ie>
2023-01-08 21:25:51 +0000

    Added new Class Testing to enter a filename, finished exercise

--- src/main/java/ie/atu/week8/FirstPart.java
+++ src/main/java/ie/atu/week8/FirstPart.java
@@ -3,19 +3,18 @@ import java.io.*;
 
 public class FirstPart {
     public static void main(String[] args) {
-        {
-            File myFile = new File("myFile1.txt"); //Creates file and gives it a name
-            System.out.println("My file is located at " + myFile.getAbsolutePath()); //File method
-            String content = "Details to write";
-            try {
-                PrintWriter myWriter = new PrintWriter("myFile1.txt");  //Class used to print any form of data
-                myWriter.println("Chris");
-                myWriter.println("Kath");
-                myWriter.println("Jean");
-                myWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+        File myFile = new File("myFile1.txt"); //Creates file and gives it a name
+        System.out.println("My file is located at " + myFile.getAbsolutePath()); //File method
+        String content = "Details to write";
+        try {
+            PrintWriter myWriter = new PrintWriter("myFile1.txt");  //Class used to print any form of data
+            myWriter.println("Chris");
+            myWriter.println("Kath");
+            myWriter.println("Jean");
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
